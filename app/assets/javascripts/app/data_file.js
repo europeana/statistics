0ff81434112de58171859062f13541296843b052
afzal-lakdawala <al@pykih.com>
2014-01-09 07:49:34 +0530

    Star visulization working now

--- app/assets/javascripts/app/data_file.js
+++ app/assets/javascripts/app/data_file.js
@@ -103,7 +103,6 @@ function generate_article_chart() {
   $(".wmd-input").hide();
   var converter = new Showdown.converter();
   var chart_types = { "Pie Chart" : "pie", "Election Donut Chart": "election-donut" , "Donut Chart": "donut", "Bar Chart": "bar", "Column Chart": "column", "Grouped Column Chart": "grouped-column" , "Line Chart": "line" }
-  var edata;
 
   $.each(gon.cms_articles, function(index, data) {
     
@@ -156,5 +155,60 @@ function generate_article_chart() {
     }
 
   });
+
+}
+
+
+function generate_article_star_chart() {
+
+  $(".wmd-input").hide();
+  var converter = new Showdown.converter();
+  var chart_types = { "Pie Chart" : "pie", "Election Donut Chart": "election-donut" , "Donut Chart": "donut", "Bar Chart": "bar", "Column Chart": "column", "Grouped Column Chart": "grouped-column" , "Line Chart": "line" }
+
+  var data = gon.star_article;    
+  var class_name = "#star-article-preview";
+  var title = data.description.split( "![visualization]\(" );
+
+  if (title.length > 0) {
+    title = title[1].split("\)")[0];
+  }
+
+  if (title.length > 0) {
+    $("<div>")
+      .attr("id", title)
+      .css({
+        "height": "300px",
+        "width": "200px"
+      })
+    .appendTo(class_name)
+
+    $.get("/generate/chart/"+title,function(vdata,status){
+
+      if (vdata) {
+        dw.visualize({
+          type: chart_types[vdata.chart_type] + "-chart", 
+          theme: 'default', 
+          container: $('#'+title),
+          datasource:   dw.datasource.delimited({csv: vdata.mapped_output})        
+        })          
+      }
+
+    });
+
+  }
+
+  if (title.length <= 0) {
+    console.log("adfsss")
+    var image_checker = converter.makeHtml(data.description);
+    $(class_name).html(image_checker);        
+    var element_type = $(class_name+" img:first")
+
+    if (element_type.length <= 0) {
+      element_type = image_checker;
+    }
+
+    $(class_name).html(element_type);
+  }
+
 
 }
