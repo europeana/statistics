2f1c8aa4f84cfb6a18d01929e140d7b9f23f622e
afzal-lakdawala <al@pykih.com>
2014-04-01 14:49:53 +0530

    Data option of menu will showned after login done

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(:version => 20140331070956) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.string   "slug"
-    t.integer  "core_tag_id"
-    t.boolean  "is_star"
     t.string   "tag"
     t.boolean  "home_page"
     t.integer  "position"
@@ -48,16 +46,6 @@ ActiveRecord::Schema.define(:version => 20140331070956) do
     t.string   "name"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
-  end
-
-  create_table "core_tags", :force => true do |t|
-    t.string   "genre"
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "slug"
-    t.integer  "sort_order"
   end
 
   create_table "data_filzs", :force => true do |t|
