8f66fa45b2a39501bf8dad7a92fff1791711a33e
amitbadheka <ab@pykih.com>
2014-10-04 15:40:20 +0530

    removed commented code from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# userlist = [{"email"=> "al@pykih.com", "password" => "afzal199"},
-#             {"email"=> "mirko@jplusplus.org", "password" => "europeana*#"},
-#             {"email"=> "neil.bates@kb.nl", "password" => "europeana*#"},
-#           ]
+userlist = [{"email"=> "al@pykih.com", "password" => "afzal199"},
+            {"email"=> "mirko@jplusplus.org", "password" => "europeana*#"},
+            {"email"=> "neil.bates@kb.nl", "password" => "europeana*#"},
+          ]
 
-# User.delete_all            
-# userlist.each do |user|
-#   user_save = User.new(email: user["email"], password: user["password"])
-#   user_save.save!
-# end
+User.delete_all            
+userlist.each do |user|
+  user_save = User.new(email: user["email"], password: user["password"])
+  user_save.save!
+end
 
-# Core::Oauth.delete_all
-# Core::Oauth.create(name: "Europeana", profile: "25899454", refresh_token: "1/2H6qnOsH0gcrJA_TQPJhffWmAkj-jCuu7uj3iwv0bUY")
-# Core::Oauth.create(name: "Europeana 1914-1918", profile: "43980802", refresh_token: "1/2H6qnOsH0gcrJA_TQPJhffWmAkj-jCuu7uj3iwv0bUY")
+Core::Oauth.delete_all
+Core::Oauth.create(name: "Europeana", profile: "25899454", refresh_token: "1/2H6qnOsH0gcrJA_TQPJhffWmAkj-jCuu7uj3iwv0bUY")
+Core::Oauth.create(name: "Europeana 1914-1918", profile: "43980802", refresh_token: "1/2H6qnOsH0gcrJA_TQPJhffWmAkj-jCuu7uj3iwv0bUY")
 
 Setting.delete_all
 json_data = {
